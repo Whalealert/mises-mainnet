2fee63b4c4b93244f3fc854d1bda159bace729e7
Dwentz™ <108969749+Whalealert@users.noreply.github.com>
2022-11-23 11:03:00 +0700

    Update mises.sh

--- mises.sh
+++ mises.sh
@@ -90,7 +90,7 @@ sed -i -e "s/^pruning-keep-every *=.*/pruning-keep-every = \"$pruning_keep_every
 sed -i -e "s/^pruning-interval *=.*/pruning-interval = \"$pruning_interval\"/" $HOME/.misestm/config/app.toml
 
 # set minimum gas price and timeout commit
-sed -i -e "s/^minimum-gas-prices *=.*/minimum-gas-prices = \"0.000025ulyl\"/" $HOME/.misestm/config/app.toml
+sed -i -e "s/^minimum-gas-prices *=.*/minimum-gas-prices = \"0.000025umis\"/" $HOME/.misestm/config/app.toml
 
 # enable prometheus
 sed -i -e "s/prometheus = false/prometheus = true/" $HOME/.misestm/config/config.toml
